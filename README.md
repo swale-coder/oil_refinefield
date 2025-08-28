3fb95837e342f9ca54cc355032076b144cadcf47
gitlabs9 <swalesiddiqui505@gmail.com>
2025-08-28 10:35:51 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ironoilfield
+This is a website I developed for a client company dealing with construction projects
---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+        else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+
+const observerItem = document.querySelectorAll('.hidden');
+observerItem.forEach((el) => {observer.observe(el)});
